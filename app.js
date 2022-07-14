a2718865e4890a1439a1c608649d04d8ee71ef48
Hasan Özdişçi <hasanozdisci@gmail.com>
2022-07-14 19:58:57 +0300

    problem fixed

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const container = document.querySelector('#container')
 const baseURL = 'https://raw.githubusercontent.com/jaylynch/pokemoji/master/img/'
 
 for (let i = 1; i < 150; i++) {
-    const pokemon = document.createElement('div');
+    const pokemon = document.createElement('div')
     pokemon.classList.add('pokemon')
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
+    const pokemonNumbers = document.createElement('span')
+    pokemonNumbers.innerText = `#${i}`
+    const pokemonImages = document.createElement('img')
+    pokemonImages.src = `${baseURL}${i}.png`
 
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
+    pokemon.appendChild(pokemonImages)
+    pokemon.appendChild(pokemonNumbers)
     container.appendChild(pokemon)
 }
 
